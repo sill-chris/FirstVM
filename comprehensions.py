994f8a06ab6642219f76075a21bac4dcf559b276
christophersill <christophersill@mail.weber.edu>
2017-12-13 09:38:56 -0700

    Add Stuff

--- comprehensions.py
+++ comprehensions.py
@@ -10,10 +10,25 @@ def is_prime(x):
     return True
 
 
+def first(iterable):
+    iterator = iter(iterable)
+    try:
+        return next(iterator)
+    except StopIteration:
+        return ValueError("Iterable is empty")
+
+
+def gen246():
+    print("About to yield 2")
+    yield 2
+    print("About to yield 4")
+    yield 4
+    print("About to yield 6")
+    yield 6
+
+
 def main():
-    """
-    Test Function
-    """
+
     l = [x for x in range(101) if is_prime(x)]
     print(l)
 
